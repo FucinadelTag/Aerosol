3696e06a965984884af09a6395a2fa9976e17e6f
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-01-29 13:50:44 +0100

    Fix safai

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,6 @@
                 <nuxt/>
             </div>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js" charset="utf-8"></script>
-        <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key="MDBmMGFjMzItODhhNy00OWYzLThkOTktYzU4MGNjNjg5NmQ1NjM2MTk2NTM1MzM1ODI2MDUy"></script>
     </div>
 
 </template>
--- plugins/snipcartFdt.js
+++ plugins/snipcartFdt.js
@@ -1,6 +1,16 @@
 export default ({ app }) => {
 
     var addthisScript = document.createElement('script');
+    addthisScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js');
+    document.body.appendChild(addthisScript);
+
+    var addthisScript = document.createElement('script');
+    addthisScript.setAttribute('src', 'https://cdn.snipcart.com/scripts/2.0/snipcart.js');
+    addthisScript.setAttribute('id', 'snipcart');
+    addthisScript.setAttribute('data-api-key', 'MDBmMGFjMzItODhhNy00OWYzLThkOTktYzU4MGNjNjg5NmQ1NjM2MTk2NTM1MzM1ODI2MDUy');
+    document.body.appendChild(addthisScript);
+
+    var addthisScript = document.createElement('script');
     addthisScript.setAttribute('src', 'https://s3-eu-west-1.amazonaws.com/cdn.fucinadeltag.it/snipcart/it.js');
     document.body.appendChild(addthisScript);
 
